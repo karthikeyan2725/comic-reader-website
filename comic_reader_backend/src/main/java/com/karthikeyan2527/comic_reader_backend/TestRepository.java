e8e3062c117f76a8c988f5dfd7ad87bd14f15a98
Karthik <yourboykarthi@gmail.com>
2025-12-08 10:04:19 +0530

    Basic Working Backend and Frontend

--- comic_reader_backend/src/main/java/com/karthikeyan2527/comic_reader_backend/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.karthikeyan2527.comic_reader_backend;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<TestEntity, Integer> {
-
-}
