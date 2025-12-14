13ca1d21fd2fe9c71641cd30f2f185da2be0ea33
Karthik <yourboykarthi@gmail.com>
2025-12-14 13:28:46 +0530

    Did not add BE files in prev commit

--- comic_reader_backend/src/main/java/com/karthikeyan2527/comic_reader_backend/controller/UserController.java
+++ comic_reader_backend/src/main/java/com/karthikeyan2527/comic_reader_backend/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.karthikeyan2527.comic_reader_backend.controller;
 
+import com.karthikeyan2527.comic_reader_backend.dto.AuthDTO;
 import com.karthikeyan2527.comic_reader_backend.entity.User;
 import com.karthikeyan2527.comic_reader_backend.service.UserService;
 import lombok.extern.slf4j.Slf4j;
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Slf4j
 @CrossOrigin("http://localhost:5173") // TODO: Make Global or use Application Yaml
@@ -18,21 +21,21 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/sign-in")
-    ResponseEntity<?> signInMethod(@RequestBody User user) { // TODO: Convert to User/Login DTO
-        Boolean isSignedIn = userService.signIn(user);
+    ResponseEntity<?> signInMethod(@RequestBody AuthDTO authDTO) { // TODO: Convert to User/Login DTO
+        Optional<String> optionalToken = userService.signIn(authDTO);
 
-        if(isSignedIn) return new ResponseEntity<>(HttpStatus.OK);
+        if(optionalToken.isPresent()) return ResponseEntity.ok(optionalToken.get());
 
         return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
     }
 
     @PostMapping("/sign-up")
-    ResponseEntity<?> signUpMethod(@RequestBody User user){ // TODO: Convert to User/Login DTO
+    ResponseEntity<?> signUpMethod(@RequestBody AuthDTO authDTO){ // TODO: Convert to User/Login DTO
         // TODO: Handle response for Conflict properly
 
-        Boolean isSignedUp = userService.signUp(user);
+        Optional<String> optionalToken = userService.signUp(authDTO);
 
-        if(isSignedUp) return new ResponseEntity<>(HttpStatus.OK);
+        if(optionalToken.isPresent()) return ResponseEntity.ok(optionalToken.get());
 
         return new ResponseEntity<>(HttpStatus.CONFLICT);
     }
--- comic_reader_backend/src/main/java/com/karthikeyan2527/comic_reader_backend/service/UserService.java
+++ comic_reader_backend/src/main/java/com/karthikeyan2527/comic_reader_backend/service/UserService.java
@@ -1,7 +1,11 @@
 package com.karthikeyan2527.comic_reader_backend.service;
 
+import com.karthikeyan2527.comic_reader_backend.dto.AuthDTO;
 import com.karthikeyan2527.comic_reader_backend.entity.User;
 import com.karthikeyan2527.comic_reader_backend.repository.UserDao;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -9,6 +13,10 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Base64;
+import java.util.Date;
 import java.util.Optional;
 
 @Service
@@ -23,26 +31,55 @@ public class UserService {
     @Autowired
     PasswordEncoder passwordEncoder;
 
-    public Boolean signIn(User user){
+    @Autowired
+    ModelMapper modelMapper;
+
+    public Optional<String> signIn(AuthDTO authDTO){
         try {
+            User user = modelMapper.map(authDTO, User.class);
             Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword()));
-            return true;
+
+            // TODO: Pass Authentication object to generateToken? Refactor bad code here
+            user = userDao.findByEmail(user.getEmail()).get();
+
+            String token = generateToken(user);
+
+            return Optional.of(token);
         } catch (Exception e) {
-            return false;
+            return Optional.empty();
         }
     }
 
-    public Boolean signUp(User user){
-        if(user.getEmail().isEmpty()) return false; // TODO: Differentiate Null user and user present failure, Return Enum?
+    public Optional<String> signUp(AuthDTO authDTO){
+        User user = modelMapper.map(authDTO, User.class);
+
+        if(user.getEmail().isEmpty()) return Optional.empty(); // TODO: Differentiate Null user and user present failure, Return Enum?
 
         Optional<User> optionalUser = userDao.findByEmail(user.getEmail()); // TODO: userDao.checkIfUserNameUsed(), refactor
 
-        if(optionalUser.isPresent()) return false;
+        if(optionalUser.isPresent()) return Optional.empty();
 
         String encryptedPassword = passwordEncoder.encode(user.getPassword());
         user.setPassword(encryptedPassword);
-        userDao.save(user);
+        user = userDao.save(user);
 
-        return true;
+        String token = generateToken(user);
+
+        return Optional.of(token);
+    }
+
+    public String generateToken(User user){
+
+        Instant instant = Instant.now();
+        byte[] secret = Base64.getDecoder().decode("SGVsbG9UaGlzSXNDb21pY1JlYWRlcldlYnNpdGVNYWRlV2l0aExvdmVCeUthcnRoaWtleWFuQW5kVGhpc0lzVXNlZFRvU2lnblRoZUhhc2hPZkpXVA==");
+        String token = Jwts.builder()
+                .subject(user.getId().toString())
+                .claim("email", user.getEmail())
+                .issuedAt(Date.from(instant))
+                .expiration(Date.from(instant.plus(1, ChronoUnit.DAYS)))
+                .signWith(Keys.hmacShaKeyFor(secret))
+                .compact();
+
+        return token;
     }
 }
